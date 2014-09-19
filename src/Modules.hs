819ee7b616784c96a35384da620deea78a5f60e4
Tobias G. W. <tobiasgwaaler@gmail.com>
2014-09-19 10:32:18 +0000

    ...

--- src/Modules.hs
+++ src/Modules.hs
@@ -1,1 +1,13 @@
 module Modules where
+
+import Modules.SubModule                     -- everything
+import Modules.SubModule (subModuleFunction) -- subModuleFunction only
+
+import qualified Modules.SubModule as S      -- everything, as S
+import qualified Modules.SubModule           -- everything, as Modules.SubModule
+
+
+qualified = Modules.SubModule.subModuleFunction
+qualifiedWithAlias = S.subModuleFunction
+
+
---
+++ src/Modules/SubModule.hs
@@ -0,0 +1,6 @@
+module Modules.SubModule (subModuleFunction) where
+{-             ^          ^
+            submodule    exported function
+-}
+subModuleFunction = 32897492
+
