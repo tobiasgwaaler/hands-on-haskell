3a97712a1b9bf0bae1e5442656d4e46d8b18bfa2
Runar Furenes <ruf@knowit.no>
2014-09-22 14:48:36 +0200

    Adding tests

--- src/Tests.hs
+++ src/Tests.hs
@@ -9,7 +9,10 @@ import qualified Functions as Functions
 import qualified PatternMatching as PM
 import qualified Recursion       as Recursion
 import qualified ModulesAndImports as MaM
-
+import qualified QuickCheckExamples   as QCE
+import qualified ProjectEuler         as PE
+import qualified HigherOrderFunctions as HOF
+import           Test.QuickCheck
 
 exercises = [
     ("Basics myName", Basics.myName /= "Tobias"),
@@ -27,17 +30,31 @@ exercises = [
     ("Recursion exercise 1: aZcZ", Recursion.containsUpperCaseZ "aZcZ" == True),
     ("Recursion exercise 1: abc", Recursion.containsUpperCaseZ "abc" == False),
 
-    ("ModulesAndImports", (MaM.myName == Basics.myName))
+    ("ModulesAndImports", (MaM.myName == Basics.myName)),
+
+    ("Project Euler - Problem 1", (PE.problem1 == 233168))
     ]
     where confCmd = PM.Configure (PM.Flags flags)
           installCmd = PM.Install (PM.Flags flags)
           runCmd = PM.Run PM.Server
           flags = ["flag"]
 
+-- QuickCheck properties
+encodeDecodeProperty :: [String] -> Bool
+encodeDecodeProperty xs = xs == (QCE.decode . QCE.encode) xs
+
+rot13Property :: String -> Bool
+rot13Property xs = xs == (QCE.rot13 . QCE.rot13) xs
+
+unionProperty :: Int -> Bool
+unionProperty n = HOF.explicitSet n == HOF.unionedSet n
 
 main = do
     putStrLn $ "Hi, " ++ Basics.myName ++ ", here are your results:"
     mapM_ check exercises
+    --quickCheck unionProperty
+    --quickCheck encodeDecodeProperty
+    --quickCheck rot13Property
 
 check :: (String, Bool) -> IO ()
 check (name,result) = putStrLn $ (if result then "√" else "x") ++ space ++ name
