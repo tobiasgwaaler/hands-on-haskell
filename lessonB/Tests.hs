da248a01a420bbe036f73933c7afc061fb171b9d
Runar Furenes <ruf@knowit.no>
2014-10-25 16:48:59 +0200

    Generating number pairs for better testing

--- lessonB/Tests.hs
+++ lessonB/Tests.hs
@@ -5,39 +5,36 @@ import Data.Bits (xor)
 import qualified Test.QuickCheck as QC
 import Test.Hspec
 
-positives :: QC.Gen Integer
-positives = 
- do -- Pick an arbitrary integer:
-    x <- QC.arbitrary 
-    -- Make it positive, if necessary:
-    if (x == 0) 
-      then return 1
-    else if (x < 0)
-      then return (-x)
-    else 
-      return x
-
+positivePairs :: QC.Gen (Integer, Integer)
+positivePairs = 
+    do x <- QC.arbitrary
+       y <- QC.arbitrary
+       return (abs x, abs y)
 
 main = hspec $ do
 
     describe "TextCalculator.textSum" $ do
-        it "sums easy summed numbers" $ do
+        it "sums numbers with no carrying needed" $ do
             TC.textSum "123" "456" `shouldBe` "579"
 
     describe "TextCalculator.textSum" $ do
-        it "sums hard summed numbers" $ do
+        it "sums numbers where carrying is needed" $ do
             TC.textSum "129" "456" `shouldBe` "585"
 
     describe "TextCalculator.textSum" $ do
-        it "sums hard summed numbers" $ do
+        it "sums numbers where carrying is needed" $ do
             TC.textSum "48" "960" `shouldBe` "1008"
 
     describe "TextCalculator.textSum" $ do
         it "sums random (positive) numbers" $ do
-            QC.quickCheck $ QC.forAll positives (\x -> TC.textSum (show x) (show x) == (show (x + x)))
+            QC.quickCheck $ QC.forAll positivePairs (\(x, y) -> TC.textSum (show x) (show y) == (show (x + y)))
 
     describe "TextCalculator.textMul" $ do
-        it "multiplication" $ do
+        it "multiplicates 48 with 10" $ do
             TC.textMul "48" "10" `shouldBe` "480"
+
+    describe "TextCalculator.textMul" $ do
+        it "multiplicates random (positive) numbers" $ do
+            QC.quickCheck $ QC.forAll positivePairs (\(x, y) -> TC.textMul (show x) (show y) == (show (x * y)))
 
 
