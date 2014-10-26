9dd9104c45f4c5d6848eeb598d7ea05c5519c0a7
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2014-10-26 19:34:54 +0100

    finnished slides and exercises for two topics

---
+++ lessonB/Bx_TypeAlias.hs
@@ -0,0 +1,20 @@
+module Bx_TypeAlias where
+
+{-
+    With type aliases you can give any type an additional name.
+    The type checker will happily ignore any type mismatches
+    if the types are defined as type aliases, but it can greatly improve
+    readability. See if you can improve the readability of the following function
+    by creating type aliases:
+-}
+
+type Response = String
+
+httpPost :: String -> String -> Integer -> String -> Response
+httpPost url contentType timeout body = undefined
+
+
+{-
+    (this exercises isn't verified with a unit test, because such a test is 
+    impossible to write)
+-}
