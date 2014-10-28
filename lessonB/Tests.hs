b062caf18e23445728de0f264fe0f4684b4eb9dc
Erlend Hamberg <erlend@hamberg.no>
2014-10-28 09:07:35 +0100

    `Maybe` tasks + tests

--- lessonB/Tests.hs
+++ lessonB/Tests.hs
@@ -1,7 +1,10 @@
 module Main where
 
+import Prelude hiding (Maybe (..))
 import qualified IntBST
 import IntBST (IntBST (..))
+import qualified Maybe
+import Maybe (Maybe (..))
 import Data.List (foldl', nub, sort)
 import qualified Test.QuickCheck as QC
 import Test.Hspec
@@ -24,12 +27,43 @@ main = hspec $ do
 
     describe "IntBST.inorder" $ do
         it "make sure an in-order traversal ouputs a sorted list" $ do
-            QC.quickCheck propSorted
-      where propSorted :: [Int] -> Bool
-            propSorted xs = IntBST.inorder (foldl' (flip IntBST.insert) Nil xs) == (sort . nub) xs
+            let propSorted xs = IntBST.inorder (foldl' (flip IntBST.insert) Nil xs) == (sort . nub) xs
+             in QC.quickCheck propSorted
 
---    describe "TextCalculator.textMul" $ do
---        it "multiplicates random (positive) numbers" $ do
---            QC.quickCheck $ QC.forAll positivePairs (\(x, y) -> TC.textMul (show x) (show y) == (show (x * y)))
---
+    describe "Maybe.safeDiv" $ do
+        it "make sure divide by non-zero denominator returns the same result as `div`" $ do
+            let propDiv :: Int -> Int -> Bool
+                propDiv _ 0 = True
+                propDiv a b = case Maybe.safeDiv a b of
+                                   Just r -> r == a `div` b
+                                   Nothing -> False
+             in QC.quickCheck propDiv
 
+    describe "Maybe.safeDiv" $ do
+        it "make sure divide by zero returns `Nothing`" $ do
+            Maybe.safeDiv 1 0 `shouldBe` Nothing
+
+    describe "Maybe.safeHead" $ do
+        it "make sure that safeHead returns the same result as `head` for non-empty lists" $ do
+            let propHead :: [Int] -> Bool
+                propHead [] = True
+                propHead xs = case Maybe.safeHead xs of
+                                   Just r -> r == head xs
+                                   Nothing -> False
+             in QC.quickCheck propHead
+
+    describe "Maybe.safeHead" $ do
+        it "make sure `safeHead` returns `Nothing` for an empty list" $ do
+            Maybe.safeHead ([] :: [Int]) `shouldBe` Nothing
+
+    describe "Maybe.f I" $ do
+        it "make sure the equation returns the correct result" $ do
+            Maybe.f 4 0 `shouldBe` Nothing
+
+    describe "Maybe.f II" $ do
+        it "make sure the equation returns the correct result" $ do
+            Maybe.f 4 4 `shouldBe` Nothing
+
+    describe "Maybe.f I" $ do
+        it "make sure the equation returns the correct result" $ do
+            Maybe.f 4 2 `shouldBe` Just 1
