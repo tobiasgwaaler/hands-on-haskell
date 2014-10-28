1652302546b74f9f9b3551e3cb0e3df1e3818f52
Runar Furenes <ruf@knowit.no>
2014-10-28 21:41:05 +0100

    Added subtraction function and tests

--- lessonB/TextCalculator.hs
+++ lessonB/TextCalculator.hs
@@ -4,7 +4,6 @@ import Data.Char
 
 type DigitList    = [Int]
 type Calculation  = String -> String -> String
-type Calculations = [String] -> String
 
 stringNumberToDigitList :: String -> DigitList
 stringNumberToDigitList = map digitToInt
@@ -12,33 +11,37 @@ stringNumberToDigitList = map digitToInt
 digitListToString :: DigitList -> String
 digitListToString = map intToDigit
 
-sortByLength :: ([a], [a]) -> ([a], [a])
-sortByLength (xs, ys) = (longer, shorter)
-                        where (longer, shorter) = if   (length xs > length ys)
-                                                  then (xs, ys)
-                                                  else (ys, xs)
+calculationNotYetImplemented :: String -> String -> String -> String
+calculationNotYetImplemented x y op = x ++ op  ++ y ++ " is not yet implemented"
 
-align :: DigitList -> DigitList -> [(Int, Int)]
-align xs ys = zip longer shorterFilled
-    where (longer, shorter) = sortByLength (xs, ys)
-          shorterFilled     = replicate diff 0 ++ shorter
-          diff              = abs $ length longer - length shorter
+{-
+    Exercise 1
+    Remember how you added numbers when you were a child? Aligning two numbers, then adding two-by-two from right to left,
+    sometimes also with a 'carry'?
+    Try to do this in Haskell - considering two numbers as Strings (which are lists of Char) like this:
 
+    123 + 456 = 123
+                456
+              = 579
+
+    If this seems hard, try to make a very simple function first, as the tests for this function vary in requirements.
+-}
 textSum :: Calculation
-textSum x y = digitListToString $ reverse $ textSumHelp alignedAndReversed 0
-              where alignedAndReversed = reverse $ align (stringNumberToDigitList x) (stringNumberToDigitList y)
+textSum x y = calculationNotYetImplemented x y "+" -- TODO Replace with your implementation
 
-textSumHelp ((x, y):rest) carry = result : textSumHelp rest newCarry
-                                  where originalSum        = x + y + carry
-                                        (result, newCarry) = if   originalSum > 9 
-                                                             then (originalSum `mod` 10, 1)
-                                                             else (originalSum, 0)
-textSumHelp _ 1 = [1]
-textSumHelp _ _ = []
+{-
+    Exercise 2
+    Do the same with subtraction.
+-}
+textSub :: Calculation
+textSub x y = calculationNotYetImplemented x y "-" -- TODO Replace with your implementation
 
-textSums :: Calculations
-textSums = foldl textSum ""
-
+{-
+    Exercise 3
+    Do the same for multiplication.
+    Hint/suggestion: Re-use textSum.
+-}
 textMul :: Calculation
-textMul x y = x ++ " X " ++ y -- TODO Fix
+textMul x y = calculationNotYetImplemented x y "*" -- TODO Replace with your implementation
+
 
