fd86a7726736a05ffe1b9eed03e1bb4b160c24cf
Runar Furenes <ruf@knowit.no>
2014-10-29 16:04:49 +0100

    Provided a simple solution for textSum

--- lessonB/TextCalculator.hs
+++ lessonB/TextCalculator.hs
@@ -14,6 +14,18 @@ digitListToString = map intToDigit
 calculationNotYetImplemented :: String -> String -> String -> String
 calculationNotYetImplemented x y op = x ++ op  ++ y ++ " is not yet implemented"
 
+sortByLength :: ([a], [a]) -> ([a], [a])
+sortByLength (xs, ys) = (longer, shorter)
+                        where (longer, shorter) = if   (length xs > length ys)
+                                                  then (xs, ys)
+                                                  else (ys, xs)
+
+align :: DigitList -> DigitList -> [(Int, Int)]
+align xs ys = zip longer shorterFilled
+    where (longer, shorter) = sortByLength (xs, ys)
+          shorterFilled     = replicate diff 0 ++ shorter
+          diff              = abs $ length longer - length shorter
+
 {-
     Exercise 1
     Remember how you added numbers when you were a child? Aligning two numbers, then adding two-by-two from right to left,
@@ -27,8 +39,16 @@ calculationNotYetImplemented x y op = x ++ op  ++ y ++ " is not yet implemented"
     If this seems hard, try to make a very simple function first, as the tests for this function vary in requirements.
 -}
 textSum :: Calculation
-textSum x y = calculationNotYetImplemented x y "+" -- TODO Replace with your implementation
+textSum x y = digitListToString $ reverse $ textSumHelp alignedAndReversed 0
+              where alignedAndReversed = reverse $ align (stringNumberToDigitList x) (stringNumberToDigitList y)
 
+textSumHelp ((x, y):rest) carry = result : textSumHelp rest newCarry
+                                  where originalSum        = x + y + carry
+                                        (result, newCarry) = if   originalSum > 9 
+                                                             then (originalSum `mod` 10, 1)
+                                                             else (originalSum, 0)
+textSumHelp _ 1 = [1]
+textSumHelp _ _ = []
 {-
     Exercise 2
     Do the same with subtraction.
