d5c7bf996831a4ee724b010d4597690ef8507141
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2014-11-08 15:17:53 +0100

    created scaffolding for lessonC

---
+++ lessonC/Tests.hs
@@ -0,0 +1,20 @@
+module Main where
+
+{-
+
+
+
+    You're not supposed to touch this ;)
+
+
+
+
+-}
+
+import qualified Test.QuickCheck as QC
+import Test.Hspec
+
+main = hspec $ do
+    describe "TODO" $ do
+        it "Write tests :)" $ do
+            1 `shouldBe` 1
