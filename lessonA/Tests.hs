aa37c6dfe51cc6417e997a703c9b9aa211529db4
Erlend Hamberg <erlend@hamberg.no>
2015-01-21 20:24:34 +0100

    Add some more Proj. Euler exercises and use `_YOUR_CODE_HERE` placeholders

--- lessonA/Tests.hs
+++ lessonA/Tests.hs
@@ -117,7 +117,19 @@ main = hspec $ do
 
     describe "Project Euler - Problem 1" $ do
         it "is correct" $ do
-            PE.problem1 `shouldBe` 233168
+            PE.problem1 `shouldBe` (2^4 * 13 * 19 * 59)
+
+    describe "Project Euler - Problem 4" $ do
+        it "is correct" $ do
+            PE.problem4 `shouldBe` (3 * 11 * 83 * 331)
+
+    describe "Project Euler - Problem 6" $ do
+        it "is correct" $ do
+            PE.problem6 `shouldBe` (2 * 3 * 5^2 * 11 * 101 * 151)
+
+    describe "Project Euler - Problem 16" $ do
+        it "is correct" $ do
+            PE.problem16 `shouldBe` (2 * 683)
 
     describe "HigherOrderFunctions.union" $ do
         it "should return union" $ do
