4f5c82c50057a6a07a8cccd89ed142aeaa435dc4
Erlend Hamberg <erlend@hamberg.no>
2015-01-21 20:09:30 +0100

    Use `_YOUR_CODE_HERE` placeholders

--- lessonA/A2_Functions.hs
+++ lessonA/A2_Functions.hs
@@ -51,7 +51,7 @@ printWelcomeMessage2 = putStrLn welcome "Welcome to the present"
 -}
 multiply arg1 arg2 = arg1 * arg2
 -- This function returns the product of its arguments
-multiply10by20 = 0
+multiply10by20 = _YOUR_CODE_HERE
 
 
 {-
@@ -59,7 +59,7 @@ multiply10by20 = 0
     Define a function, plus, that takes 2 arguments and returns their sum
 -}
 plus :: Integer -> Integer -> Integer
-plus arg1 arg2 = 0
+plus arg1 arg2 = _YOUR_CODE_HERE
 
 
 {-
@@ -68,7 +68,7 @@ plus arg1 arg2 = 0
     ... and you must use the plus function to do so!
 -}
 sum3 :: Integer -> Integer -> Integer -> Integer
-sum3 arg1 arg2 arg3 = 0
+sum3 arg1 arg2 arg3 = _YOUR_CODE_HERE
 
 
 {-
@@ -77,7 +77,7 @@ sum3 arg1 arg2 arg3 = 0
     True only if that character is a dollar sign ($).
 -}
 isDollar :: Char -> Bool
-isDollar character = undefined
+isDollar character = _YOUR_CODE_HERE
 
 
 {-
@@ -85,6 +85,6 @@ isDollar character = undefined
     Define an "exclusive or" operator: http://en.wikipedia.org/wiki/Exclusive_or#Truth_table
 -}
 xor :: Bool -> Bool -> Bool
-xor arg1 arg2 = False
+xor arg1 arg2 = _YOUR_CODE_HERE
 
-
+_YOUR_CODE_HERE = undefined -- ignore me
